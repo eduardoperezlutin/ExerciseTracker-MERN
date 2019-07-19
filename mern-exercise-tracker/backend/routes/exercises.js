fbfbffe0d13bde5fe751d575e083c613d5120458
eduardoperezlutin <eduardoperezlutin@gmail.com>
2019-07-19 15:28:44 -0600

    CRUD

--- mern-exercise-tracker/backend/routes/exercises.js
+++ mern-exercise-tracker/backend/routes/exercises.js
@@ -25,4 +25,34 @@ router.route('/add').post((req, res) => {
     .catch(err => res.status(400).json(`Error: ${err}`));
 });
 
+// get single exercise
+router.route('/:id').get((req, res) => {
+  Exercise.findById(req.params.id)
+    .then(exercise => res.json(exercise))
+    .catch(err => res.status(400).json(`Error: ${err}`));
+});
+
+// delete
+router.route('/:id').delete((req, res) => {
+  Exercise.findByIdAndDelete(req.params.id)
+    .then(exercise => res.json(`Exercise deleted -> ${exercise.id}`))
+    .catch(err => res.status(400).json(`Error: ${err}`));
+});
+
+// update
+router.route('/update/:id').post((req, res) => {
+  Exercise.findByIdAndUpdate(req.params.id)
+    .then(exercise => {
+      exercise.username = req.body.username;
+      exercise.description = req.body.description;
+      exercise.duration = req.body.duration;
+      exercise.date = req.body.date;
+
+      exercise.save()
+        .then(() => res.json("Exercise updated!"))
+        .catch(err => res.status(400).json(`Error: ${err}`));
+    })
+    .catch(err => res.status(400).json(`Error: ${err}`));
+})
+
 module.exports = router;
--- mern-exercise-tracker/backend/routes/users.js
+++ mern-exercise-tracker/backend/routes/users.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
 let User = require('../models/user.model');
 
+// get all
 router.route('/').get((req, res) => {
   User.find()
     .then(users => res.json(users))
     .catch(err => res.status(400).json(`Error: ${err}`));
 });
 
+// create
 router.route('/add').post((req, res) => {
   const username = req.body.username;
 
@@ -16,5 +18,32 @@ router.route('/add').post((req, res) => {
     .then(() => res.json('User added!'))
     .catch(err => res.status(400).json(`Error: ${err}`));
 });
+
+// get single user
+router.route('/:id').get((req, res) => {
+  User.findById(req.params.id)
+    .then(user => res.json(user))
+    .catch(err => res.status(400).json(`Error: ${err}`));
+});
+
+// delete
+router.route('/:id').delete((req, res) => {
+  User.findByIdAndDelete(req.params.id)
+    .then(user => res.json(`User deleted -> ${user.username}`))
+    .catch(err => res.status(400).json(`Error: ${err}`));
+});
+
+// update
+router.route('/update/:id').post((req, res) => {
+  User.findByIdAndUpdate(req.params.id)
+    .then(user => {
+      user.username = req.body.username;
+
+      user.save()
+        .then(() => res.json("User updated!"))
+        .catch(err => res.status(400).json(`Error: ${err}`));
+    })
+    .catch(err => res.status(400).json(`Error: ${err}`));
+})
 
 module.exports = router;
