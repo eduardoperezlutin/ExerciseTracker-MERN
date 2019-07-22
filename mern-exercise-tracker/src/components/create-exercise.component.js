0f32f921ca11dfdb5fda997d1a931163cd1a6cc5
eduardoperezlutin <eduardoperezlutin@gmail.com>
2019-07-22 12:28:14 -0600

    redirect after exercise creation

--- mern-exercise-tracker/src/components/create-exercise.component.js
+++ mern-exercise-tracker/src/components/create-exercise.component.js
@@ -74,7 +74,7 @@ export default class CreateExercises extends Component {
     axios.post('http://localhost:5000/exercises/add', exercise)
       .then(res => console.log(res.data));
 
-    // window.location = '/';
+    window.location = '/';
   }
 
   render() {
